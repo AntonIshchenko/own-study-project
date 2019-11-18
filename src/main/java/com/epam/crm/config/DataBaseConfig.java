eae8acabc430752fa7f8d879f5a576d12ae4f08e
Anton Ishchenko <anton_ishchenko@epam.com>
2019-11-18 13:38:38 +0300

    Continue configuretion

--- src/main/java/com/epam/crm/config/DataBaseConfig.java
+++ src/main/java/com/epam/crm/config/DataBaseConfig.java
@@ -1,17 +1,24 @@
 package com.epam.crm.config;
 
+import javax.sql.DataSource;
 import org.h2.Driver;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 @PropertySource(value = "classpath:sql/db-config.yml")
 @EnableAspectJAutoProxy
-public class DataBaseConfig extends WebMvcConfigurationSupport {
+public class DataBaseConfig {
 
   @Value("${db.url}")
   private String url;
@@ -24,7 +31,32 @@ public class DataBaseConfig extends WebMvcConfigurationSupport {
   @Value("${classpath:sql/schema.sql}")
   private Resource schemaScript;
 
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(dataSource());
+  }
 
+  @Bean
+  public DataSource dataSource() {
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    dataSource.setDriver(driver);
+    dataSource.setUsername(userName);
+    dataSource.setPassword(password);
+    dataSource.setUrl(url);
+    return dataSource;
+  }
 
+  @Bean
+  public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+    final DataSourceInitializer initializer = new DataSourceInitializer();
+    initializer.setDataSource(dataSource);
+    initializer.setDatabasePopulator(databasePopulator());
+    return initializer;
+  }
 
+  private DatabasePopulator databasePopulator() {
+    ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+    populator.addScript(schemaScript);
+    return populator;
+  }
 }
